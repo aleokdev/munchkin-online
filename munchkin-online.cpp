23a1d1ab4b3f1626cc784b4883ebf1d7efed87a8
alexdevteam <alexpro820@gmail.com>
2019-12-28 00:23:46 +0100

    Add project files.

---
+++ munchkin-online.cpp
@@ -0,0 +1,12 @@
+﻿// munchkin-online.cpp : Defines the entry point for the application.
+//
+
+#include "munchkin-online.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
