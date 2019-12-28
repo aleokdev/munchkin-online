a73ca6928a9df729861526978d9c3c752b34eec4
alexdevteam <alexpro820@gmail.com>
2019-12-28 14:18:35 +0100

    Partial commit

--- main.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <iostream>
-
-int main()
-{
-	std::cout << "insert game here" << std::endl;
-	std::cin.get();
-	return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <SDL.h>
+
+int main()
+{
+	std::cout << "insert game here" << std::endl;
+	if (!SDL_Init(SDL_INIT_EVERYTHING))
+	{
+		std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
+		std::cin.get();
+	}
+	SDL_Window* window = SDL_CreateWindow("Munchkin Online", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 600, NULL);
+
+	SDL_Event event;
+	bool close;
+	do
+	{
+		SDL_PollEvent(&event);
+		close = event.type == SDL_QUIT;
+	} while (!close);
+
+	return 0;
+}
