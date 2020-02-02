e5810ac3e5fd3e32bda75bc5172c5c808dc1af99
alexdevteam <alexpro820@gmail.com>
2020-02-02 20:50:45 +0100

    Close #72

--- src/systems/debug_terminal.cpp
+++ src/systems/debug_terminal.cpp
@@ -14,23 +14,8 @@ DebugTerminal::DebugTerminal(Game& g) : game(&g) {
 
 void DebugTerminal::log_lua(sol::this_state state, sol::object obj) {
     // @todo: Make log_lua also log the print caller
-    switch (obj.get_type()) {
-        case (sol::type::boolean):
-            std::cout << ((bool)obj ? "true" : "false") << std::endl;
-            terminal_log.emplace_back((bool)obj ? "true" : "false");
-            break;
-
-        case (sol::type::string):
-            std::cout << obj.as<std::string>() << std::endl;
-            terminal_log.emplace_back(obj.as<std::string>());
-            break;
-
-        case (sol::type::number):
-            std::cout << obj.as<int>() << std::endl;
-            terminal_log.emplace_back(std::to_string(obj.as<int>()));
-            break;
-            // TODO: Handle all switch cases
-    }
+    std::cout << obj.as<std::string>() << std::endl; 
+    terminal_log.emplace_back(obj.as<std::string>());
 }
 
 void DebugTerminal::render() {
@@ -40,10 +25,10 @@ void DebugTerminal::render() {
     ImGui::BeginChildFrame(ImGui::GetID("debug_frame"), ImGui::GetContentRegionAvail());
     ImGui::SetScrollY(ImGui::GetScrollMaxY());
     for (auto& str : terminal_log) ImGui::TextUnformatted(str.c_str());
-    ImGui::EndChildFrame();
+            ImGui::EndChildFrame();
 
-    ImGui::End();
-}
+            ImGui::End();
+    }
 
 } // namespace systems
-} // namespace munchkin
+} // namespace systems
